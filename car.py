7d92e6730820a74ee03aafa37639405317bee6e0
Yann Schüler <yschueler@stud.macromedia.de>
2023-06-08 11:54:57 +0200

    changed from a .db to .json format

---
+++ car.py
@@ -0,0 +1,8 @@
+class Car:
+
+    def __init__(self, brand, mileage, model, color):
+        self.brand = brand
+        self.mileage = mileage
+        self.model = model
+        self.color = color
+        self.status = "free"
