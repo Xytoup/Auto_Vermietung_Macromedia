1b8d595f776c768ddb4f96f75245d2bd0d7b38e7
Yann Schüler <yschueler@stud.macromedia.de>
2023-06-11 14:33:27 +0200

    beautified the menu

--- database.py
+++ database.py
@@ -7,16 +7,20 @@ from customer import Customer
 class Database:
     @classmethod
     def save_all_cars(cls, filename, car_list):
+        # Convert car objects to dictionaries
         cars = [car.to_dict() for car in car_list]
+        # Save the car data to a JSON file
         with open(filename, "w") as file:
             json.dump(cars, file, indent=4)
 
     @classmethod
     def load_all_cars(cls, filename):
+        # Load car data from the JSON file
         with open(filename, "r") as file:
             cars_data = json.load(file)
 
         car_list = []
+        # Create car objects from the loaded data and add them to the car list
         for car in cars_data:
             car_obj = Car(
                 brand=car["brand"],
@@ -35,32 +39,42 @@ class Database:
 
     @classmethod
     def delete_car_by_id(cls, car_id, car_list):
+        # Iterate over the car list
         for car in car_list:
+            # Find the car with the matching ID
             if car.get_id() == int(car_id):
+                # Remove the car from the list
                 car_list.remove(car)
                 print(f"Car with ID '{car_id}' has been deleted.")
                 break
         else:
+            # If no car with the given ID is found
             print(f"Car with ID '{car_id}' not found.")
 
     @classmethod
     def add_car(cls, brand, mileage, model, color, rental_price, location, year, id, car_list, status="available"):
+        # Create a new car object
         new_car = Car(brand, mileage, model, color, rental_price, location, year, id, status)
+        # Add the new car to the car list
         car_list.append(new_car)
         print("New car added successfully.")
 
     @classmethod
     def save_all_customers(cls, filename, customer_list):
+        # Convert customer objects to dictionaries
         customers = [customer.to_dict() for customer in customer_list]
+        # Save the customer data to a JSON file
         with open(filename, "w") as file:
             json.dump(customers, file, indent=4)
 
     @classmethod
     def load_all_customers(cls, filename):
+        # Load customer data from the JSON file
         with open(filename, "r") as file:
             customers_data = json.load(file)
 
         customer_list = []
+        # Create customer objects from the loaded data and add them to the customer list
         for customer in customers_data:
             customer_obj = Customer(
                 name=customer["name"],
@@ -74,16 +88,20 @@ class Database:
 
     @classmethod
     def add_customer(cls, name, age, id, customer_list):
+        # Create a new customer object
         new_customer = Customer(name, age, id)
+        # Add the new customer to the customer list
         customer_list.append(new_customer)
         print("New customer added successfully.")
 
     @classmethod
     def get_all_customer_ids(cls, customer_list):
+        # Get a list of all customer IDs
         return [customer.id for customer in customer_list]
 
 
 def list_cars(loaded_cars):
+    # Display information for each car in the loaded car list
     for car in loaded_cars:
         print("------------------------------------")
         print("Brand:", car.get_brand())
@@ -94,6 +112,7 @@ def list_cars(loaded_cars):
 
 
 def list_customers(customer_list):
+    # Display information for each customer in the customer list
     for customer in customer_list:
         print("------------------------------------")
         print("Name:", customer.get_name())
@@ -104,61 +123,86 @@ def list_customers(customer_list):
 
 
 def get_customer_by_id(customer_list, customer_id):
+    # Find the customer with the given ID in the customer list
     for customer in customer_list:
         if str(customer.get_id()) == str(customer_id):
             return customer
+    # If no customer with the given ID is found
     print("Customer with ID {} not found.".format(customer_id))
     return None
 
 
-
 def show_car_by_id(loaded_cars, car_id):
+    # Find the car with the given ID in the loaded car list
     for car in loaded_cars:
         if car.get_id() == int(car_id):
+            # Display car information
             print("------------------------------------")
             print("Brand:", car.get_brand())
-            print("model: ", car.get_model())
+            print("Model: ", car.get_model())
             print("Year: ", car.get_year())
             print("Mileage: ", car.get_mileage())
             print("Color: ", car.get_color())
-            print("1 day rental price ", car.get_rental_price(), "€")
+            print("1 day rental price: ", car.get_rental_price(), "€")
             print("Location: ", car.get_location())
             print("ID: ", car.get_id())
             print("Status: ", car.get_status())
             print("------------------------------------")
+            return  # Exit the function if car ID is found
+
+    # If no car with the given ID is found
+    print("Car with ID", car_id, "not found.")
 
 
 def get_all_car_ids(car_list):
+    # Get a list of all car IDs
     return [car.get_id() for car in car_list]
 
 
 def get_car_by_id(car_list, car_id):
+    # Find the car with the given ID in the car list
     for car in car_list:
         if car.get_id() == car_id:
             return car
+    # If no car with the given ID is found
     return None
 
 
 def check_in(customer, car):
+    # Set the rented car for the customer to None
     customer.set_rented_car(None)
+    # Set the car's status to available
     car.set_available()
 
 
+def price_calc(car):
+    # Calculate the outstanding amount for renting the car
+    print("How many days did the customer rent the car?")
+    days = int(input("> "))
+    print(f"Outstanding amount: {days * car.get_rental_price()}€")
+
+
 def car_id_generator(car_list):
+    # Generate a random car ID and check if it is already in use
     new_id = random.randint(1000, 9999)
     existing_ids = [car.get_id() for car in car_list]
 
     if new_id in existing_ids:
+        # If the generated ID is already in use, generate a new ID recursively
         return car_id_generator(car_list)
     else:
+        # If the generated ID is unique, return it
         return new_id
 
 
 def customer_id_generator(customer_list):
+    # Generate a random customer ID and check if it is already in use
     new_id = random.randint(1000, 9999)
     existing_ids = [customer.id for customer in customer_list]
 
     if new_id in existing_ids:
+        # If the generated ID is already in use, generate a new ID recursively
         return customer_id_generator(customer_list)
     else:
+        # If the generated ID is unique, return it
         return new_id
--- menu.py
+++ menu.py
@@ -1,16 +1,43 @@
 import database
 from database import *
-from customer import *
-from car import *
+import os
 
 # Load all cars from the JSON file
 loaded_cars = Database.load_all_cars("car_list.json")
 loaded_customers = Database.load_all_customers("customer_list.json")
 
+# Add ASCII art and styling to the main menu
+MAIN_MENU_ART = """
+-------------------------------------
+         CAR RENTAL SYSTEM
+-------------------------------------
+"""
 
+# Add ASCII art and styling to the vehicle management menu
+VEHICLE_MENU_ART = """
+-------------------------------------
+       VEHICLE MANAGEMENT MENU
+-------------------------------------
+"""
+
+# Add ASCII art and styling to the customer management menu
+CUSTOMER_MENU_ART = """
+-------------------------------------
+       CUSTOMER MANAGEMENT MENU
+-------------------------------------
+"""
+
+# Function to clear the screen
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# Function to display the main menu
 def main_menu():
     allowed = ["v", "c", "x"]
     while True:
+        clear_screen()
+        print(MAIN_MENU_ART)
         print("What do you want to do?")
         print("- V for vehicle management")
         print("- C for customer management")
@@ -20,107 +47,143 @@ def main_menu():
         if choice.lower() in allowed:
             return choice
         else:
-            print("wrong input")
+            print("Invalid input. Please try again.")
 
 
+# Function to display the vehicle management menu
 def vehicle_menu():
     allowed = ["s", "a", "i", "d", "x"]
     while True:
+        clear_screen()
+        print(VEHICLE_MENU_ART)
         print("What do you want to do?")
         print("- S to show all cars")
         print("- A to add a new car")
         print("- I to search a specific car by its ID")
         print("- D to delete a car by its ID")
-        print("- x to return to the main menu")
+        print("- X to return to the main menu")
 
         choice = input("> ")
 
         if choice in allowed:
+            # Perform actions based on user choice
             match choice.lower():
                 case "s":
-                    list_cars(loaded_cars)
+                    list_cars(loaded_cars)  # Call function to list all cars
                 case "a":
                     Database.add_car(
                         input("Brand > "), int(input("Mileage > ")), input("Model > "), input("Color > "),
                         int(input("Rental price > ")), input("Location > "), int(input("Year > ")),
                         car_id_generator(loaded_cars), loaded_cars
-                    )
+                    )  # Call function to add a new car
                 case "i":
                     show_car_by_id(loaded_cars, input("Please state the ID of the car you want to search: "))
+                    # Call function to show a specific car by its ID
                 case "d":
                     Database.delete_car_by_id(input("Please state the ID of the car you want to delete: "), loaded_cars)
+                    # Call function to delete a car by its ID
                 case "x":
-                    break
+                    break  # Exit the vehicle menu
         else:
-            print("wrong input")
+            print("Invalid input. Please try again.")
 
 
+# Function to display the customer management menu
 def customer_menu():
-    allowed = ["s", "a", "r", "x"]
+    allowed = ["s", "a", "r", "x", "c"]
     while True:
+        clear_screen()
+        print(CUSTOMER_MENU_ART)
         print("What do you want to do?")
         print("- S to show all customers")
         print("- A to add a new customer")
         print("- R to rent out a car")
         print("- C to check a car back in")
-        print("- x to return to the main menu")
+        print("- X to return to the main menu")
 
         choice = input("> ")
 
         if choice.lower() not in allowed:
-            print("Wrong input")
+            print("Invalid input. Please try again.")
 
+        # Perform actions based on user choice
         match choice.lower():
             case "s":
-                list_customers(loaded_customers)
+                list_customers(loaded_customers)  # Call function to list all customers
             case "a":
-                add_new_customer()
+                add_new_customer()  # Call function to add a new customer
             case "r":
-                rent_out_car()
+                rent_out_car()  # Call function to rent out a car
             case "c":
-                check_in_car()
+                check_in_car()  # Call function to check in a car
             case "x":
-                break
+                break  # Exit the customer menu
 
 
 def add_new_customer():
-    name = input("Name > ")
-    age = int(input("Age > "))
+    print(CUSTOMER_MENU_ART)
+    name = input("Name: ")
+    age = int(input("Age: "))
     customer_id = customer_id_generator(loaded_customers)
-    Database.add_customer(name, age, customer_id, loaded_customers)
+    Database.add_customer(name, age, customer_id, loaded_customers)  # Call function to add a new customer
 
 
 def rent_out_car():
+    # Get the list of customer IDs
     customer_ids = Database.get_all_customer_ids(loaded_customers)
+
+    # Get the list of car IDs
     car_ids = database.get_all_car_ids(loaded_cars)
 
+    # ASCII art for the rent out car prompt
+    RENT_OUT_CAR_PROMPT_ART = """
+-------------------------------------
+         RENT OUT A CAR
+-------------------------------------
+"""
+
     while True:
-        print(customer_ids)
+        print(RENT_OUT_CAR_PROMPT_ART)
+        print("Quick customer ID list:", customer_ids)
+
+        # Prompt the user to enter the customer ID or return to the menu
         choice = input("Enter the customer ID or press X to return to the menu: ")
 
         if choice.lower() == "x":
             break
+
+        # Check if the entered customer ID is valid
         elif int(choice) in customer_ids:
             print(f"Customer with ID {choice} selected")
             selected_customer = get_customer_by_id(loaded_customers, choice)
 
+            # Check if the customer already has a car rented
             if selected_customer.get_rented_car() is not None:
                 print("This customer already has a car rented.")
                 print("------------------------------------")
                 break
 
-            print(car_ids)
+            print("Quick car ID list:", car_ids)
+
+            # Prompt the user to enter the car's ID or return to the menu
             choice = input("Enter the car's ID or press X to return to the menu: ")
+
             if choice.lower() == "x":
                 break
 
+            # Check if the entered car ID is valid
             elif int(choice) in car_ids:
                 selected_car = get_car_by_id(loaded_cars, int(choice))
+
+                # Check if the car is already rented
                 if selected_car.check_availability():
                     print("This car is already rented.")
                 else:
+                    # Assign the car to the customer and mark it as rented
                     selected_customer.set_rented_car(choice)
                     selected_car.set_rented()
+                    print("The car has been given to the customer")
+                    print("------------------------------------")
                     break
             else:
                 print("Invalid car ID.")
@@ -129,38 +192,86 @@ def rent_out_car():
 
 
 def check_in_car():
+    # Get the list of customer IDs
     customer_ids = Database.get_all_customer_ids(loaded_customers)
+
+    # Get the list of car IDs
     car_ids = database.get_all_car_ids(loaded_cars)
 
+    # ASCII art for the check in car prompt
+    CHECK_IN_CAR_PROMPT_ART = """
+-------------------------------------
+         CHECK IN A CAR
+-------------------------------------
+"""
+
     while True:
-        print(customer_ids)
+        print(CHECK_IN_CAR_PROMPT_ART)
+        print("Quick customer ID list:", customer_ids)
+
+        # Prompt the user to enter the customer ID or return to the menu
         choice = input("Enter the customer ID or press X to return to the menu: ")
 
         if choice.lower() == "x":
             break
+
+        # Check if the entered customer ID is valid
         elif int(choice) in customer_ids:
             print(f"Customer with ID {choice} selected")
             selected_customer = get_customer_by_id(loaded_customers, choice)
 
+            # Check if the customer has a rented car
             if selected_customer.get_rented_car() is None:
                 print("This customer doesn't have a car rented.")
                 print("------------------------------------")
                 break
 
-            print(car_ids)
+            print("Quick car ID list:", car_ids)
+
+            # Prompt the user to enter the car's ID or return to the menu
             choice = input("Enter the car's ID or press X to return to the menu: ")
 
             if choice.lower() == "x":
                 break
+
+            # Check if the entered car ID is valid
             elif int(choice) in car_ids:
                 selected_car = get_car_by_id(loaded_cars, int(choice))
 
+                # Check if the car is currently rented
                 if not selected_car.check_availability():
                     print("This car is not currently rented.")
                 else:
+                    # Perform the check-in process for the car
                     check_in(selected_customer, selected_car)
+                    print("The car has been returned")
+                    print("------------------------------------")
+                    price_calc(selected_car)
+                    print("------------------------------------")
+                    print("Confirm the receipt of the payment with Y")
+
+                    while True:
+                        # Prompt the user to confirm the payment receipt
+                        if input("> ").lower() == "y":
+                            print("Payment Confirmed")
+                            print("------------------------------------")
+                            break
+                        else:
+                            print("Payment has not been confirmed")
                     break
             else:
                 print("Invalid car ID.")
         else:
             print("Invalid customer ID.")
+
+
+# Call the main menu function to start the program
+main_choice = main_menu()
+
+while main_choice.lower() != "x":
+    if main_choice.lower() == "v":
+        vehicle_menu()
+    elif main_choice.lower() == "c":
+        customer_menu()
+
+    main_choice = main_menu()
