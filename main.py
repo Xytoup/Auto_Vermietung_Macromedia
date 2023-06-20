34a12ecd30a1266cf3d817507d92bc14755c9391
Yann Schüler <yschueler@stud.macromedia.de>
2023-06-20 13:07:29 +0200

    added "if __name__ == "__main__":"

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from menu import *
 
-while True:
-    choice = main_menu().lower()
+if __name__ == "__main__":
+    while True:
+        choice = main_menu().lower()
 
-    if choice == "v":
-        vehicle_menu(loaded_cars)  # Pass the loaded cars as an argument
-    elif choice == "c":
-        customer_menu(loaded_customers)  # Pass the loaded customers as an argument
-    elif choice == "x":
-        database.save_all(loaded_cars, loaded_customers)
-        break
+        if choice == "v":
+            vehicle_menu(loaded_cars)  # Pass the loaded cars as an argument
+        elif choice == "c":
+            customer_menu(loaded_customers)  # Pass the loaded customers as an argument
+        elif choice == "x":
+            database.save_all(loaded_cars, loaded_customers)
+            break
